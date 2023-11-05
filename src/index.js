05fb0c1659b672fb144d761301b5459731bc68ff
mmdmthr <muhhari@gmail.com>
2023-11-05 21:21:56 +0700

    component props

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function Car() {
-    return <h2>Hi, I am a car!</h2>
+function Car(props) {
+    return <h2>I am a {props.color} car!</h2>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Car />);
+root.render(<Car color="red"/>);
