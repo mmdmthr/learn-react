6f499779a55f3342b74429adbeab9b1fe59d0628
mmdmthr <muhhari@gmail.com>
2023-11-08 19:38:21 +0700

    conditional rendering

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Car from './Car';
 
-function Football() {
-    const shoot = (a, b) => {
-        alert(b.type);
-        /*
-        'b' represents the React event that triggered the function,
-        in this case the 'click' event
-        */
+function MissedGoal() {
+    return <h3>Missed!</h3>;
+}
+
+function MadeGoal() {
+    return <h3>Goal!</h3>;
+}
+
+function Goal(props) {
+    const isGoal = props.isGoal;
+    if (isGoal) {
+        return <MadeGoal />;
     }
-
-    return (
-        <button onClick={(event) => shoot("Goal!", event)}>Take the shoot!</button>
-    )
+    return <MissedGoal />;
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Football />);
+root.render(<Goal isGoal={true} />);
