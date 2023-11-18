715a010272037b2f20055ac45da93f2a02a64d10
mmdmthr <muhhari@gmail.com>
2023-11-18 19:48:27 +0700

    react memo before

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-import Blogs from "./pages/Blogs";
-import Contact from "./pages/Contact";
-import NoPage from "./pages/NoPage";
+import { useState } from "react";
+import ReactDOM from "react-dom/client";
+import Todos from "./Todos";
 
-export default function App() {
+const App = () => {
+    const [count, setCount] = useState(0);
+    const [todos, setTodos] = useState(["todo 1", "todo 2"]);
+
+    const increment = () => {
+        setCount((c) => c + 1);
+    }
+
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Home />} />
-                    <Route path="blogs" element={<Blogs />} />
-                    <Route path="contact" element={<Contact />} />
-                    <Route path="*" element={<NoPage />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
+        <>
+            <Todos todos={todos} />
+            <hr />
+            <div>
+                Count: {count}
+                <button onClick={increment}>+</button>
+            </div>
+        </>
     )
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
