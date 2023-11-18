9b3c9581eeb1bd88717f38ca3581605419dcbbb0
mmdmthr <muhhari@gmail.com>
2023-11-18 19:55:01 +0700

    react memo after

--- src/Todos.js
+++ src/Todos.js
@@ -1,3 +1,5 @@
+import { memo } from "react";
+
 const Todos = ({todos}) => {
     console.log("child render");
     return (
@@ -10,4 +12,4 @@ const Todos = ({todos}) => {
     )
 }
 
-export default Todos;
+export default memo(Todos);
