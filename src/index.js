bf4e6f21767e5472aa612b0379b44c72c9df321b
mmdmthr <muhhari@gmail.com>
2023-11-19 09:30:19 +0700

    react hook

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import './my-sass.scss';
 
-const Header = () => {
+function FavoriteColor() {
+    const [color, setColor] = useState("red");
     return (
-      <>
-        <h1>Hello Style!</h1>
-        <p>Add a little style!.</p>
-      </>
+        <div>
+            <h1>My favorite color is {color}</h1>
+            <button
+                type="button"
+                onClick={() => setColor("blue")}
+            >Blue</button>
+            <button
+                type="button"
+                onClick={() => setColor("red")}
+            >Red</button>
+            <button
+                type="button"
+                onClick={() => setColor("pink")}
+            >Pink</button>
+            <button
+                type="button"
+                onClick={() => setColor("green")}
+            >Green</button>
+        </div>
     );
-  }
+}
   
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(<Header />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<FavoriteColor />);
