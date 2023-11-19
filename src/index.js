6d8333557dea538882bfb0ff06f8e9497759194d
mmdmthr <muhhari@gmail.com>
2023-11-19 09:54:51 +0700

    react useState on array and object

--- src/index.js
+++ src/index.js
@@ -1,30 +1,33 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import ReactDOM from "react-dom/client";
 
-function FavoriteColor() {
-    const [color, setColor] = useState("red");
-    return (
-        <div>
-            <h1>My favorite color is {color}</h1>
-            <button
-                type="button"
-                onClick={() => setColor("blue")}
-            >Blue</button>
-            <button
-                type="button"
-                onClick={() => setColor("red")}
-            >Red</button>
-            <button
-                type="button"
-                onClick={() => setColor("pink")}
-            >Pink</button>
-            <button
-                type="button"
-                onClick={() => setColor("green")}
-            >Green</button>
-        </div>
-    );
+function Car() {
+  const [car, setCar] = useState({
+    brand: "Ford",
+    model: "Mustang",
+    year: "1964",
+    color: "red"
+  });
+
+  const updateColor = () => {
+    setCar(previousState => {
+      return { ...previousState, color: "blue" }
+    });
+  }
+
+  return (
+    <>
+      <h1>My {car.brand}</h1>
+      <p>
+        It is a {car.color} {car.model} from {car.year}.
+      </p>
+      <button
+        type="button"
+        onClick={updateColor}
+      >Blue</button>
+    </>
+  )
 }
-  
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<FavoriteColor />);
+root.render(<Car />);
