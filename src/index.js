e0b08edb374d9bf1f9eea28c2dfea42cc13e27de
mmdmthr <muhhari@gmail.com>
2023-11-19 19:50:10 +0700

    react custom hook fetch

--- src/index.js
+++ src/index.js
@@ -1,44 +1,25 @@
-import { useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 
-const App = () => {
-    const [count, setCount] = useState(0);
-    const [todos, setTodos] = useState([]);
-    const calculation = useMemo(() => expensiveCalculation(count), [count]);
+const Home = () => {
+    const [data, setData] = useState(null);
 
-    const increment = () => {
-        setCount((count) => count + 1);
-    };
-    
-    const addTodo = () => {
-        setTodos((todos) => [...todos, "New Todo"]);
-    };
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/todos")
+            .then((res) => res.json())
+            .then((data) => setData(data));
+    }, []);
 
     return (
         <>
-            <h2>My Todos</h2>
-            {todos.map((todo, index) => {
-                return <p key={index}>{todo}</p>
-            })}
-            <button onClick={addTodo}>Add Todo</button>
-            <hr />
-            <div>
-                Count: {count}
-                <button onClick={increment}>+</button>
-                <h2>Expensive Calculation</h2>
-                {calculation}
-            </div>
+            {data &&
+                data.map((item) => {
+                    return <p key={item.id}>{item.title}</p>
+                })
+            }
         </>
     )
-};
-
-const expensiveCalculation = (num) => {
-    console.log("Calculating...");
-    for (let i = 0; i < 1000000000; i++) {
-        num += 1;
-    }
-    return num;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<Home />);
