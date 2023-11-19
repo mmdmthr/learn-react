b534e0202ed9241bd801b48ad6cfc556635d08f8
mmdmthr <muhhari@gmail.com>
2023-11-19 18:51:42 +0700

    react useRef hook accessing DOM elements

--- src/index.js
+++ src/index.js
@@ -2,21 +2,19 @@ import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-    const [inputValue, setInputValue] = useState("");
-    const count = useRef(0);
-
-    useEffect(() => {
-        count.current = count.current + 1;
-    });
+    const inputElement = useRef();
+    
+    const focusInput = () => {
+        inputElement.current.focus();
+    }
 
     return (
         <>
             <input
                 type="text"
-                value={inputValue}
-                onChange={(e) => setInputValue(e.target.value)} 
+                ref={inputElement}
             />
-            <h1>Render Count: {count.current}</h1>
+            <button onClick={focusInput}>Focus Input</button>
         </>
     )
 }
