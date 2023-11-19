d1cd4e42db2197f65b4e026117d210dc5c4183ee
mmdmthr <muhhari@gmail.com>
2023-11-19 19:45:52 +0700

    react useMemo expensive calculation

--- src/index.js
+++ src/index.js
@@ -1,29 +1,43 @@
-import { useCallback, useReducer, useState } from "react";
+import { useMemo, useState } from "react";
 import ReactDOM from "react-dom/client";
-import Todos from "./Todos";
 
 const App = () => {
     const [count, setCount] = useState(0);
     const [todos, setTodos] = useState([]);
+    const calculation = useMemo(() => expensiveCalculation(count), [count]);
 
     const increment = () => {
         setCount((count) => count + 1);
     };
     
-    const addTodo = useCallback(() => {
+    const addTodo = () => {
         setTodos((todos) => [...todos, "New Todo"]);
-    }, [todos]);
+    };
 
     return (
         <>
-            <Todos todos={todos} addTodo={addTodo} />
+            <h2>My Todos</h2>
+            {todos.map((todo, index) => {
+                return <p key={index}>{todo}</p>
+            })}
+            <button onClick={addTodo}>Add Todo</button>
             <hr />
             <div>
                 Count: {count}
                 <button onClick={increment}>+</button>
+                <h2>Expensive Calculation</h2>
+                {calculation}
             </div>
         </>
     )
+};
+
+const expensiveCalculation = (num) => {
+    console.log("Calculating...");
+    for (let i = 0; i < 1000000000; i++) {
+        num += 1;
+    }
+    return num;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
