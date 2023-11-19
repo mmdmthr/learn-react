11105be0424c561eaab43b7c1587b62260792d59
mmdmthr <muhhari@gmail.com>
2023-11-19 10:14:52 +0700

    react hook useEffect with dependency variable

--- src/index.js
+++ src/index.js
@@ -3,14 +3,19 @@ import ReactDOM from "react-dom/client";
 
 function Timer() {
     const [count, setCount] = useState(0);
+    const [calculation, setCalculation] = useState(0);
 
     useEffect(() => {
-        setTimeout(() => {
-            setCount((count) => count + 1);
-        }, 1000);
-    }, []); // <- add empty array for dependency
+        setCalculation(() => count * 2);
+    }, [count]); // <- add empty array for dependency
 
-    return <h1>I've rendered {count} times</h1>
+    return (
+        <>
+            <p>Count: {count}</p>
+            <button onClick={() => setCount((count) => count + 1)}>+</button>
+            <p>Calculation: {calculation}</p>
+        </>
+    )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
