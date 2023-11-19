73d5acee84fbe8a6d7c5af7f23ccbbba9a8bc5f3
mmdmthr <muhhari@gmail.com>
2023-11-19 10:34:58 +0700

    react useEffect cleanup

--- src/index.js
+++ src/index.js
@@ -3,19 +3,16 @@ import ReactDOM from "react-dom/client";
 
 function Timer() {
     const [count, setCount] = useState(0);
-    const [calculation, setCalculation] = useState(0);
 
     useEffect(() => {
-        setCalculation(() => count * 2);
-    }, [count]); // <- add empty array for dependency
+        let timer = setTimeout(() => {
+            setCount((count) => count + 1);
+        }, 1000);
 
-    return (
-        <>
-            <p>Count: {count}</p>
-            <button onClick={() => setCount((count) => count + 1)}>+</button>
-            <p>Calculation: {calculation}</p>
-        </>
-    )
+        return () => clearTimeout(timer);
+    }, []);
+
+    return <h1>I've rendered {count} times</h1>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
